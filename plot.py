4ae19bdcbb1fedf640de00a6513c4937c8f34821
arihant parsoya <parsoyaarihant@gmail.com>
2016-05-26 10:02:36 +0530

    implemented plot_contour function

---
+++ plot.py
@@ -0,0 +1,53 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.ndimage.interpolation import shift
+
+def plot_contour(cnt1=None, cnt2=None, axis1='X', axis2='Y', top_axis='Z'):
+
+    '''
+    To plot the contour in 3D with height of the person as the given axis
+
+    Inputs:
+        cnt1: first contour to be plotted
+        cnt2: second contour to be plotted
+        axis: axis along which it will be plotted in 3D
+
+    Output:
+        plots the contour in 3D
+    '''
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+
+    x1 = []
+    y1 = []
+    z1 = []
+    x2 = []
+    y2 = []
+    z2 = []
+
+    if cnt1 is not None:
+        for points in cnt1:
+            x1.append(points[0][0])
+            z1.append(-points[0][1])
+            y1.append(0)
+    x1 = np.array(x1) - np.average(x1) # shifting the plot to the origin
+
+    if cnt2 is not None:
+        for points in cnt2:
+            y2.append(points[0][0])
+            z2.append(-points[0][1])
+            x2.append(0)
+
+    y2 = np.array(y2) - np.average(y2) # shifting the plot to origin
+
+    ax.scatter(x1, y1, z1, c='r', marker='D')
+    ax.scatter(x2, y2, z2, c='r', marker='*')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from outer import encContour
 from height import height
+from plot import plot_contour
 from matplotlib import pyplot as plt
 import cv2
 import numpy as np
@@ -7,10 +8,16 @@ import copy
 import math
 import sys
 
-
-image = cv2.imread('1.jpg')
-cnt = encContour('1.jpg')
+url = '4.jpg'
+cnt = encContour(url)
+plot_contour(cnt1= cnt, cnt2= cnt, axis1='X')
+#print((cnt))
+'''
+#test for encContour
+image = cv2.imread(url)
+cnt = encContour(url)
 cv2.drawContours(image,cnt,-1,(255),3)
 cv2.imshow('image',image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
