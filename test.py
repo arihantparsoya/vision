3e75e35b964b8e7dc00151ef09406e604bf5dc98
arihant parsoya <parsoyaarihant@gmail.com>
2016-06-01 00:50:29 +0530

    added files

---
+++ test.py
@@ -0,0 +1,11 @@
+from outer import encContour
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+import copy
+import math
+import sys
+from plot3d import plot3dHuman
+from ret2Val import ret2Val
+k=ret2Val('man_front.jpg')
+plot3dHuman('man_front.jpg','man_side.jpg',k)
